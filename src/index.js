2a6fd61828d609942a75b3cdb9713e6d29569f1d
David Renshaw <renshaw@icarm.io>
2026-04-23 14:55:32 -0400

    put some basic stuff into place

--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => c.text('Hello World!'))
+app.get('/', (c) =>
+  c.html(`<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Equation 677 Database</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <header>
+      <div class="inner">
+        <h1>Equation 677 Database</h1>
+        <nav>
+          <a href="/">Home</a>
+        </nav>
+      </div>
+    </header>
+    <main>
+      <h2>Hello, world.</h2>
+      <p>Welcome to the Equation 677 Database.</p>
+    </main>
+    <footer><a href="https://icarm.io">icarm.io</a></footer>
+  </body>
+</html>
+`),
+)
 
 export default app
